6a12291599817a7d5dac751c49427ea8b5f014b5
marcos@softniels.com.br <marcos@softniels.com.br>
2023-09-08 10:15:42 -0300

    Create sample

---
+++ Unit1.pas
@@ -0,0 +1,54 @@
+﻿unit Unit1;
+
+interface
+
+uses
+  MVCFramework,
+  MVCFramework.Commons,
+  MVCFramework.Serializer.Commons,
+  MVCFramework.Nullables;
+
+type
+  TBoleto = class
+  private
+    FnossoNumero: string;
+    Ftipodesconto: NullableString;
+  public
+    property nossoNumero: string read FnossoNumero write FnossoNumero;
+    property tipodesconto: NullableString read Ftipodesconto write Ftipodesconto;
+  end;
+
+  [MVCPath('/api')]
+  TMyController = class(TMVCController)
+  public
+    [MVCPath]
+    [MVCHTTPMethod([httpGET])]
+    procedure Index;
+
+    [MVCPath('/reversedstrings/($Value)')]
+    [MVCHTTPMethod([httpGET])]
+    procedure GetReversedString(const Value: string);
+  end;
+
+implementation
+
+uses
+  System.SysUtils,
+  MVCFramework.Logger,
+  System.StrUtils;
+
+procedure TMyController.Index;
+begin
+  var Boleto := TBoleto.Create;
+  Boleto.nossoNumero := '123456789';
+  Boleto.tipodesconto := 'VALOR';
+  Render(Boleto);
+end;
+
+procedure TMyController.GetReversedString(const Value: string);
+begin
+  Render(System.StrUtils.ReverseString(Value.Trim));
+end;
+
+end.
+
